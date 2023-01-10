c6ceb17dc35f9fb23c46b542f960077dfa36942e
Nadim <nhsajib316@gmail.com>
2023-01-11 01:22:52 +0600

    Top menu and navbar web plus mobile view

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HeroImage from  '../assets/heroImage.png';
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
